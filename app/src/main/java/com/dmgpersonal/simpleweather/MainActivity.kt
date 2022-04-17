706a51fa3999b930bea94c73957d3cf905eade82
Dmitriy Gridchin <tigris.tstu@gmail.com>
2022-04-17 16:04:53 +0300

    Пока практически пустой проект, разбираюсь с mvvm и пытаюсь привыкнуть к Kotlin

--- app/src/main/java/com/dmgpersonal/simpleweather/MainActivity.kt
+++ app/src/main/java/com/dmgpersonal/simpleweather/MainActivity.kt
@@ -2,17 +2,25 @@ package com.dmgpersonal.simpleweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dmgpersonal.simpleweather.databinding.MainActivityBinding
 import com.dmgpersonal.simpleweather.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        //setContentView(R.layout.main_activity)
+        setContentView(view)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
         }
     }
 }
--- app/src/main/java/com/dmgpersonal/simpleweather/ui/main/MainFragment.kt
+++ app/src/main/java/com/dmgpersonal/simpleweather/ui/main/MainFragment.kt
@@ -6,9 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import com.dmgpersonal.simpleweather.AppState
 import com.dmgpersonal.simpleweather.R
+import com.dmgpersonal.simpleweather.databinding.MainFragmentBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = MainFragment()
@@ -16,17 +25,50 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        val view: View = binding.root
+        return view
+        //return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it) })
+        viewModel.getWeather()
+
+//        val observer = Observer<Any> { renderData(it) }
+//        viewModel.getLiveData().observe(viewLifecycleOwner, observer)
     }
 
+    private fun renderData(appState: AppState) {
+        val loadingLayout = binding.loadingLayout
+        when (appState) {
+            is AppState.Success -> {
+                //val weatherData = appState.weatherData
+                loadingLayout.visibility = View.GONE
+                //Toast.makeText(context, "Success", Toast.LENGTH_LONG).show()
+                Snackbar.make(requireView(), "Success", Snackbar.LENGTH_LONG).show()
+            }
+            is AppState.Loading -> {
+                loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                loadingLayout.visibility = View.GONE
+               // Toast.makeText(context, "Error", Toast.LENGTH_LONG).show()
+                Snackbar
+                    .make(requireView(), "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { viewModel.getWeather() }
+                    .show()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
